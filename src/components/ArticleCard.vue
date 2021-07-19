3ff3efe6760293b5ac9c389464666a5f0b40ee64
osamamhd <osamamhd82@gmail.com>
2021-07-19 04:30:09 +0200

    💥 make article card component with 3 functions (heart, like, happy)

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,119 @@
+<template>
+
+<v-flex xs12 sm6 lg3>
+    <v-card
+        class="mx-2 my-2"
+        max-width="400"
+    >
+        <v-img
+        class="white--text align-end"
+        height="150px"
+        :src="article.get_image"
+        >
+        <v-card-title>{{ article.title }}</v-card-title>
+        </v-img>
+
+        <v-card-text class="text--primary pb-0">
+        <div>{{ article.description }}</div>
+        </v-card-text>
+
+        <v-card-subtitle class="pb-0">
+        <v-icon small>local_offer</v-icon>
+        <span v-for="tag in article.tags" :key="tag">
+            {{ tag }},
+        </span>
+        </v-card-subtitle>
+
+        <v-card-actions>
+        <v-btn
+            color="teal"
+            text small
+            @click="postHeart"
+        >
+        <v-icon>favorite</v-icon>
+            <span class="mx-1">{{ heart }}</span>
+            
+        </v-btn>
+
+        <v-btn
+            class="pl-0"
+            color="teal"
+            text small
+            @click="postLike"
+        >
+        <v-icon>thumb_up</v-icon>
+            <span class="mx-1 px-0">{{ like }}</span>
+        </v-btn>
+
+        <v-btn
+            class="pl-0"
+            color="teal"
+            text small
+            @click="postHappy"
+        >
+        <v-icon>emoji_emotions</v-icon>
+            <span class="mx-1">{{ happy }}</span>
+        </v-btn>
+
+        <v-btn
+            color="teal"
+            text small 
+            router :to="article.get_absolute_url"
+        >
+            Read Now
+        </v-btn>
+        </v-card-actions>
+    </v-card>
+</v-flex>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'ArticleCard',
+
+    props: {
+        article: Object
+    }, 
+
+    data() {
+        return {
+            heart: this.article.heart,
+            like: this.article.like,
+            happy: this.article.happy,
+            id: this.article.id
+        }
+    }, 
+
+    methods: {
+       
+        postHeart() {
+            axios
+                .post(`/api/${this.id}/heart/`, this.heart)
+                .then(this.heart++)
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+
+        postLike() {
+            axios
+                .post(`/api/${this.id}/like/`, this.like)
+                .then(this.like++)
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+
+        postHappy() {
+            axios
+                .post(`/api/${this.id}/happy/`, this.happy)
+                .then(this.happy++)
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,53 @@
 <template>
 <div class="home">
-<p>Homepage</p>
+  <v-container>
+
+    <h1 class="grey--text">Articles</h1>
+    <v-layout row class="my-2">
+      <ArticleCard 
+        v-for="article in articles" 
+        :key="article.id" 
+        :article="article"/>
+    </v-layout>
+
+  </v-container>
 </div>
 
 </template>
 
 <script>
-
+import axios from 'axios'
+import ArticleCard from '@/components/ArticleCard.vue'
   export default {
     name: 'Home',
+
+    data() {
+      return {
+        articles: []
+      }
+    }, 
+
+    components: {
+      ArticleCard,
+    }, 
+
+    mounted() {
+      this.getArticles()
+      document.title = 'High Python | Articles'
+    }, 
+
+    methods: {
+      async getArticles() {
+        await axios
+          .get('/api/')
+          .then(response => {
+            this.articles = response.data
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }
+    }
 
 
   }
