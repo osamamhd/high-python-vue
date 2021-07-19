7a9f4b6d68c1ec00c85bb1f068b26995deac5fbb
osamamhd <osamamhd82@gmail.com>
2021-07-19 02:20:50 +0200

    📦 markdown packages and loaders.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [ {
+        test: /\.md$/,
+       loader: 'raw-loader', // npm install -D raw-loader
+      }]
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
