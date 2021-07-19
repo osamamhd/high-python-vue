a3efb666e3cd758bad35e1c40a605f77042114d0
osamamhd <osamamhd82@gmail.com>
2021-07-19 02:23:25 +0200

    ⚡️font awesome.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,4 +1,5 @@
 import 'material-design-icons-iconfont/dist/material-design-icons.css' 
+import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
